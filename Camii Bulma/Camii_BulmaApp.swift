729b0041759028ec0011792c48f760a24622ef39
Fatih Gümrükçü <fatih.gmrkc@gmail.com>
2025-03-07 00:41:40 +0300

    Initial Commit

---
+++ Camii Bulma/Camii_BulmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camii_BulmaApp.swift
+//  Camii Bulma
+//
+//  Created by Fatih Gümrükçü on 7.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Camii_BulmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
